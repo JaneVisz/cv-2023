ad5d6e2a389d340d60dbc518d96712962d7b14ec
Jana <jana.viszkocsilova@gmail.com>
2023-10-15 18:16:31 +0200

    Add files via upload

---
+++ myCv.js
@@ -0,0 +1,20 @@
+$(document).ready(function ($) {
+    $(document).on('click', '.btn_print', function (event) {
+        event.preventDefault();
+
+        // Get the HTML element you want to convert to PDF
+        let element = document.getElementById('cv-container');
+
+        // Define PDF options
+        let opt = {
+            margin: 0,
+            filename: 'viszkocsilova-cv.pdf', // Specify the PDF file name
+            image: {type: 'jpeg', quality: 1}, // Image options
+            html2canvas: {scale: 2}, // Scale for better resolution
+            jsPDF: {unit: 'mm', format: 'A4', orientation: 'portrait'} // A4 page format
+        };
+
+        // Use html2pdf to convert the content to PDF
+        html2pdf().from(element).set(opt).save();
+    });
+});
